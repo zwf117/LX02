12f0eaa3f4b49c05330ac4d325fa6c2085160e2c
zwf117 <61720059+zwf117@users.noreply.github.com>
2021-09-25 20:03:15 +0800

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine(DAL.UserInfo.Instance.UserCheck("Admin"));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Model.UserInfo user = new Model.UserInfo { userName = "Guest", passWord = "sql.123", type = "π‹¿Ì‘±" };
+            Console.WriteLine(DAL.UserInfo.Instance.Add(user));
+            Console.WriteLine(JsonConvert.SerializeObject(DAL.UserInfo.Instance.GetAll()));
+            user.qq = "104512708";
+            Console.WriteLine(DAL.UserInfo.Instance.Update(user));
+            var model = DAL.UserInfo.Instance.GetModel("Guest");
+            Console.WriteLine(JsonConvert.SerializeObject(model));
+            Console.WriteLine(DAL.UserInfo.Instance.Delete("Guest"));
+            var page = DAL.UserInfo.Instance.GetPage(new Model.Page { pageIndex = 2, pageSize = 2 });
+            Console.WriteLine(JsonConvert.SerializeObject(page));
+            Console.WriteLine(DAL.UserInfo.Instance.GetCount());
+        }
+    }
+}
